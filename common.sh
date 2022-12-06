c34c3e4073e0aefaafe4c1154641331a0983d689
Wen Zhou <wenzhou@redhat.com>
2022-12-06 17:06:25 +0100

    update: handle multiple upstream tags on the same GA tag 	fix: path for jdk8u need a sub-folder "workspace" 	update: rename variable

--- common.sh
+++ common.sh
@@ -11,18 +11,31 @@ function checkArgs() {
 
 function cloneGitHubRepo() {
   cd "$WORKSPACE" || exit 1
-  # If we don't have a $GITHUB_REPO locally then clone it from adoptium/$GITHUB_REPO.git
-  if [ ! -d "$GITHUB_REPO" ] ; then
-    git clone "$REPO" "$GITHUB_REPO" || exit 1
+  GITHUB_REPO=$1 # https://github.com/adoptium/adoptium/jdk17u.git
+  if [ ! -d "$JDKVERSION" ] ; then
+    echo "First time clone repo $GITHUB_REPO"
+    git clone -q "$GITHUB_REPO" || exit 1
   else
-    cd "$GITHUB_REPO" && git clean -fd && git fetch --all --tags
+    cd "$JDKVERSION" && git clean -fd && git fetch --all --tags
   fi
 }
 
 function readExpectedGATag() {
-    source ${WORKSPACE}/releasePlan.cfg
+    source ${SCRIPT_DIR}/releasePlan.cfg
     jdkVersion=$1  # e.g jdk17u
     gaTagVariable="${jdkVersion}GA" # set gaTagVariable=jdk17uGA match releasePlan.cfg
     expectedTag=${!gaTagVariable} # get value, e.g: expectedTag=jdk-17.0.6
     echo ${expectedTag}
+}
+
+function queryGHAPI(){
+  repo=$1 # adoptium/jdk8u, openjdk/jdk8u
+  tag=$2 # jdk8u362-b05_adopt, jdk8u362-ga
+  exist="$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/${repo}/git/refs/tags/${tag}")"
+  if [ $exist == "200" ]; then
+    echo "Found tag: ${tag} in ${repo}"
+  else
+    echo "Cannot find tag: ${tag} in ${repo}"
+  fi
+  echo ${exist}
 }
--- triggerReleasePipeline.sh
+++ triggerReleasePipeline.sh
@@ -12,69 +12,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+set -euo pipefail
 
 ### Context (example):
 ### upstream skara creats tags jdk8u352-b08, then add jdk8u352-ga onto the same SHA on GA day
 ### skaraMirror merge jdk8u352-b08 onto release branch and tag it as jdk8u352-b08_adopt
 ### official release use jdk8u352-b08_adopt
 
-SKARA_REPO="https://github.com/openjdk/$1"
-GITHUB_REPO="$1"
-REPO=${2:-"https://github.com/adoptium/$GITHUB_REPO.git"}
+JDKVERSION="$1"
+ADOPTIUM_REPO=${2:-"https://github.com/adoptium/$JDKVERSION.git"}
 BRANCH="master"
+
+# Since we have "jdk8u" in the code, we need to create another lever of "workspace"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $SCRIPT_DIR/workspace
+WORKSPACE=$SCRIPT_DIR/workspace
 
 # to local supporting functions
 # TODO: this should be done for skaraMirror.sh too
-source ${WORKSPACE}/common.sh
+source ${SCRIPT_DIR}/common.sh
 
 # pre-check args
 checkArgs $#
 # cleanup properties if exists from previous run
 git clean -fd # same as rm -rf ${WORKSPACE}/properties but might also clean other dirty files
 # for first time if does not have repo locally yet
-cloneGitHubRepo
+cloneGitHubRepo $ADOPTIUM_REPO
 
 # fetch all new refs including tags from origin remote
-cd "$WORKSPACE/$GITHUB_REPO"
+cd "$WORKSPACE/$JDKVERSION"
 
 # take latest -ga tag (sorted by time)
 gaTag=$(git tag --sort=-v:refname | grep '\-ga' | head -1)
 echo "latest GA tag: ${gaTag}"
 
 # read expectedTag from cfg file (releasePlan.cfg) to see if this is the correct GA tag we want for release
-expectedTag=$(readExpectedGATag $1)
+expectedTag=$(readExpectedGATag $JDKVERSION)
 
 # get the older tag name between the latest GA tag and expected tag
-olderTag=`echo -e "$expectedTag\n$gaTag" | sort -V | head -n1`
+olderTag="$(echo -e "${expectedTag}\n${gaTag}" | sort -V | head -n1)"
 # if GA tag (e.g jdk-19.0.2+5-ga) is greater or equal than expected (e.g jdk-19.0.2) => this is the correct GA we need
-if [[ $expectedTag == $olderTag || $expectedTag == $gaTag ]]; then
-  echo "$gaTag is not the GA tag we expect for this release! e.g $expectedTag-ga"
-  exit # should not continue trigger logic
+if [[ "${expectedTag}" == "${olderTag}" || "${expectedTag}" == "${gaTag}" ]]; then
+  echo "we will proceed with ${gaTag} to trigger build"
 else
-  echo "we will proceed with $gaTag to trigger build"
+  echo "${gaTag} is not the GA tag we expect for this release! We want e.g ${expectedTag}-ga or a higher GA tag"
+  exit # should not continue trigger logic
 fi
 
 # from -ga tag find original commit SHA then list all tags point to the this SHA(exclude -ga tag) => orignal tag(s) from skara
-# in rare case, there might be more tags than just the one we want
-scmReferenceList=$(git rev-list -1 ${gaTag} | xargs git tag --points-at  | grep  -v '\-ga')
-
+# in rare case, there might be more tags than the one we want
+# convert from multiple line string into Array and use the first one only
+cd "$WORKSPACE/$JDKVERSION"
+scmReferenceString="$(git rev-list -1 ${gaTag} | xargs git tag --points-at  | grep  -v '\-ga')"
+scmReferenceList=($scmReferenceString)
+# append _adopt => release tag we use in adoptium
+scmReference="${scmReferenceList[0]}_adopt"
 # loop with 10m sleep if git-mirror has not applied the _adopt tag or if there is a merge conflict in git-mirror that we need to manual resolve
 for i in {1..5}
 do
-  for scmReference in ${scmReferenceList[@]}; do
-    # append _adopt => release tag we should use in adoptium
-    scmReference=${scmReference}_adopt
-    # if there are multiple tags in scmReferenceList, only one with _adpot avaiable
-    if [ $(git tag -l "${scmReference}") ]; then
-      echo "Found tag: ${scmReference}"
-      # write into properties file for release pipeline to get input from
-      # existence of this file will be used in jenkins job as if should continue trigger logic
-        echo scmReference=$scmReference > ${WORKSPACE}/properties
-      break # should continue trigger logic
-    else
-      echo "No ${scmReference} tag found yet, will sleep 10 minutes and check again"
-      sleep 10m
-      git fetch --all --tags
-    fi
-  done
+  if [ $(git tag -l "${scmReference}") ]; then
+    echo "Found tag: ${scmReference}"
+    # write into properties file for release pipeline to get input from
+    # existence of this file will be used in jenkins job as if should continue trigger logic
+    echo scmReference=$scmReference > ${SCRIPT_DIR}/properties
+    break # should continue trigger logic
+  else
+    echo "No ${scmReference} tag found yet, will sleep 10 minutes and check again"
+    sleep 10m
+    git fetch --all --tags
+  fi
 done
