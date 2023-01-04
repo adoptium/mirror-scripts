ef02fd9deb3dd446682aaa98f327cb3233f6aa0c
Andrew Leonard <anleonar@redhat.com>
2023-01-04 10:13:01 +0000

    expectedTag gets overwritten by checkPrious scmReference
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- common.sh
+++ common.sh
@@ -45,8 +45,8 @@ function queryGHAPI(){
 # if yes, wont trigger; if not, do the first time trigger
 function checkPrevious() {
   if [ -f ${WORKSPACE}/tracking ]; then # already have tracking from previous
-    scmRef=$1 # _adopt scmReference tag we have found
-    trackerTag="$(cut -d '=' -f 2 ${WORKSPACE}/tracking)" # previousSCM=jdk-17.0.5+8_adopt
+    local scmRef=$1 # _adopt scmReference tag we have found
+    local trackerTag="$(cut -d '=' -f 2 ${WORKSPACE}/tracking)" # previousSCM=jdk-17.0.5+8_adopt
 
     if [[ "${scmRef}" == "${trackerTag}" ]]; then
         echo "Release tag ${trackerTag} has triggered a release pipeline build already in the current release"
